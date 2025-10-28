af5d4fb98f405192adbb15711166690709c98462
TharushaDenuwan <tharushadenuwan35@gmail.com>
2025-10-28 16:34:03 +0530

    updated code

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,10 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "kidlink.s3.ap-south-1.amazonaws.com"
+      },
+      {
+        protocol: "https",
+        hostname: "donext-org.s3.eu-west-2.amazonaws.com"
       }
     ]
   },
