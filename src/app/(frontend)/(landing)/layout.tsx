fe189028f2032fb4bc4b58b799651d5ae7b41a70
D.K.DeSilva <141452020+dkdesilva@users.noreply.github.com>
2025-10-29 13:51:29 +0530

    commit

--- src/app/(frontend)/(landing)/layout.tsx
+++ src/app/(frontend)/(landing)/layout.tsx
@@ -8,11 +8,11 @@ type Props = {
 
 export default function LandingLayout({ children }: Props) {
   return (
-    <div className="min-h-screen bg-slate-50">
+    <div className="min-h-screen bg-slate-50 flex flex-col">
       {/* Header - Modify with conditional classes based on scroll state */}
       <Header />
 
-      <main>{children}</main>
+      <main className="flex-1">{children}</main>
 
       <Footer />
     </div>
