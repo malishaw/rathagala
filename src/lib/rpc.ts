676ed6ec25713af74932936312deba8d77bb5d4d
Malisha <malishatds@gmail.com>
2026-08-03 22:03:41 +0530

    hnhj

--- src/lib/rpc.ts
+++ src/lib/rpc.ts
@@ -1,4 +1,6 @@
 import type { AppType } from "@/server";
 import { hc } from "hono/client";
 
-export const client = hc<AppType>(process.env.NEXT_PUBLIC_APP_URL as string);
+const appUrl = process.env.NEXT_PUBLIC_APP_URL || (typeof window !== "undefined" ? window.location.origin : "https://rathagala.lk");
+
+export const client = hc<AppType>(appUrl);
