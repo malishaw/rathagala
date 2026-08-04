62b3740de681254770d0a79f9123be511263cb64
Malisha <malishatds@gmail.com>
2026-08-04 21:31:13 +0530

    k

--- src/server/routes/ad/ad.index.ts
+++ src/server/routes/ad/ad.index.ts
@@ -3,21 +3,20 @@ import { createRouter } from "@/server/helpers/create-app";
 import * as handlers from "./ad.handler";
 import * as routes from "./ad.routes";
 
-// Now use the withAuth wrapper for the routes that need authentication
 const router = createRouter()
   .openapi(routes.list, handlers.list)
-  .openapi(routes.getOne, handlers.getOne)
   .openapi(routes.create, handlers.create)
+  .openapi(routes.trending, handlers.trending)
+  .openapi(routes.bulkCreate, handlers.bulkCreate)
+  .openapi(routes.bulkPermanentDelete, handlers.bulkPermanentDelete)
+  .openapi(routes.sendExpiryReminders, handlers.sendExpiryReminders)
+  .openapi(routes.getOne, handlers.getOne)
   .openapi(routes.update, handlers.update)
   .openapi(routes.remove, handlers.remove)
   .openapi(routes.permanentDelete, handlers.permanentDelete)
-  .openapi(routes.bulkPermanentDelete, handlers.bulkPermanentDelete)
   .openapi(routes.approve, handlers.approve)
   .openapi(routes.reject, handlers.reject)
-  .openapi(routes.bulkCreate, handlers.bulkCreate)
   .openapi(routes.incrementView, handlers.incrementView)
-  .openapi(routes.trending, handlers.trending)
-  .openapi(routes.renew, handlers.renew)
-  .openapi(routes.sendExpiryReminders, handlers.sendExpiryReminders);
+  .openapi(routes.renew, handlers.renew);
 
 export default router;
